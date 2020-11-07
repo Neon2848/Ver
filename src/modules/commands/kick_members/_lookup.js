18bafddcafa183006ffe86946d3051bc7240b995
SegFault <animus@v3rmillion.net>
2020-11-07 02:20:22 +0100

    General bugfixes and tidyups

--- src/modules/commands/kick_members/_lookup.js
+++ src/modules/commands/kick_members/_lookup.js
@@ -16,7 +16,7 @@ const sendError = (err, editable) => {
 const catchUpdateError = (e, message) => {
   let translateError = e
   if (e.message === "Cannot read property 'guild' of undefined") translateError = new Error('The user is not currently in the server')
-  if (e.message === 'Missing Permissions') translateError = new Error('This user has a higher role then me')
+  if (e.message === 'Missing Permissions') translateError = new Error('This user has a higher role than me')
   sendResult(translateError.message, { message, timeout: 5000 }, 'Not setting roles/username.')
 }
 
--- src/modules/events/ready.js
+++ src/modules/events/ready.js
@@ -4,7 +4,9 @@
 const Discord = require('discord.js') // eslint-disable-line no-unused-vars
 const mongo = require('../../mongo/connect')
 const log = require('../../mongo/log')
+const { getV3rmId } = require('../../mongo/members')
 const { getNextUnmuteMuteTime } = require('../../mongo/mute')
+const { basicLookup } = require('../functions/general')
 
 const convertNamesCircular = (inputArray, guild, setting, type) => {
   Object.keys(inputArray).forEach((key) => {
@@ -44,21 +46,3 @@ module.exports = async (client) => {
   })
   log('global', 'info', 'connected', undefined, { user: client.user.tag })
 }
-
-/* // The following temp code is just to kick pending users when the bot transfer happens.
-
-  const v3rm = client.guilds.cache.get('571114137107562541')
-  const membs = await v3rm.members.fetch()
-  const pending = membs.filter((memb) => !memb.roles.cache.find((r) => r.name === 'Member')).array()
-  const totalCount = pending.length - 1
-  const failedCount = 0
-
-  for (let i = 0; i < totalCount; i += 1) {
-    const m = pending[i]
-    const { tag } = m.user
-
-    await m.send("Hey, because you haven't activated yet, we're kicking you from the v3rmillion server to tidy things up. You're welcome to rejoin at https://v3rmillion.net/discord").catch(() => { failedCount += 1 })
-    await m.kick('Pending user kicked to cleanup server and prepare for new role system.')
-      .catch(() => console.log(`kick failed on: ${tag}`))
-    console.log(`Progress: ${i}/${totalCount}. Failed DMS: ${failedCount}`)
-  } */
