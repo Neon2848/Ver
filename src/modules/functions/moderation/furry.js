51977f1c9d54580eedb20297026b343008fb45cd
PlutonusDev <plutonus.stream@gmail.com>
2021-01-31 23:26:42 +0000

    Fixed linter errors.

--- src/modules/functions/moderation/furry.js
+++ src/modules/functions/moderation/furry.js
@@ -1,22 +1,22 @@
 const { sendResult } = require('../general')
+const { addtoRoleQueue, attemptRoleQueue } = require('../api/v3rm/userSetup')
 
 const checkFurry = async (client, message) => {
-  const guild = message.guild
-  const member = message.member
+  const { guild, member } = message
   const furryRole = await guild.roles.fetch(guild.ver.roles.furry)
   const furryStrings = ['owo', 'uwu', 'awoo']
 
-  if (!member.roles.has(furryRole) {
-    if (new RegExp(furryStrings.join("|")).test(message.content)) {
-      if(member.roles) {
+  if (!member.roles.has(furryRole)) {
+    if (new RegExp(furryStrings.join('|')).test(message.content)) {
+      if (member.roles) {
         await member.roles.add(furryRole).catch(() => {
           addtoRoleQueue(member.id, member, null, [furryRole.name])
             .then(() => attemptRoleQueue())
-        });
+        })
       }
+      sendResult(`<@${message.author.id}>, you've yee'd your last haw.`,
+        { message, timeout: 5000 }, 'You Darned Furry!')
     }
-    sendResult(`<@${message.author.id}>, you've yee'd your last haw.`,
-      { message, timeout: 5000 }, 'You Darned Furry!')
   }
 }
 
